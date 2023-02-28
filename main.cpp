d79722e236e2a2e5442f3391ec28557ee43b4789
Thomas Vivas <thomas.p.vivas@gmail.com>
2023-02-27 21:35:04 -0500

    Refactored calculation Calculations moved into it's own separate function. Switch statement replaced with anonymous functions that are mapped to using a hashmap

--- main.cpp
+++ main.cpp
@@ -1,36 +1,42 @@
 #include <iostream>
+#include <unordered_map>
+#include <functional>
 #include <iomanip>
 using namespace std;
 
-int main()
+bool divByZero(float a, float b, float c, float d) {
+    return b==0||c==0||d==0;
+}
+
+float evaluate(float a, float b, float c, float d, char op) {
+    unordered_map<char, function<float(float, float, float, float)>> operations;
+    operations = {
+        {'+', [](float a, float b, float c, float d) {return a + b + c + d;}},
+        {'-', [](float a, float b, float c, float d) {return a - b - c - d;}},
+        {'/', [](float a, float b, float c, float d) {return a / b / c / d;}},
+        {'*', [](float a, float b, float c, float d) {return a * b * c * d;}}
+    };
+    return operations[op](a, b, c, d);
+}
+
+int main(int argc, char** argv)
 {
-    float get1, get2, get3, get4;
-    char opr;
+    float a, b, c, d;
+    char op;
+
     cout << "Calculator version 1.0.0" << endl;
     cout << "Please enter 4 numbers: ";
-    cin >> get1 >> get2 >> get3 >> get4;
-    cout << "Enter operator: +, -, *, /: "; 
-    cin >> opr;
-    cout << get1 << " "<< opr << " " << get2 << " " << opr << " " << get3 << " " << opr << " " << get4;
-    switch(opr){  
-        case'+':
-            cout << get1 + get2 + get3 + get4;
-            break;
-        case '-':
-            cout << get1-get2-get3-get4;
-            break;
-        case '*':
-            cout << get1*get2*get3*get4;
-            break;
-        case '/':
-            if(get2 !=0)
-                cout << get1/get2/get3/get4;
-            else
-                cout << "Cannot divide by zero\n" << endl;
-            break;
-        default:
-            cout << "Illegal operation" << endl;
+    cin >> a >> b >> c >> d;
+
+    cout << "Enter operator: +, -, *, /: ";
+    cin >> op;
+
+    if (op == '/' && divByZero(a, b, c, d)) {
+        throw "Division by zero not permitted";
     }
+
+    cout << evaluate(a, b, c, d, op) << endl;
+
     return 0;
 }
 
